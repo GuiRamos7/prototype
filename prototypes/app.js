0f4197226dcdf7d2d16296d91708e6a371422da1
Guilherme Ramos <GuiOliveiraRamos10@gmail.com>
2018-11-12 11:36:06 -0200

    Criando funções para saber qual é o maior e o menor numero em uma array

--- prototypes/app.js
+++ prototypes/app.js
@@ -31,6 +31,56 @@ Array.prototype.isOdd = function() {
       
     }
   }
-  
+
   return newArray;
 }
+
+Array.prototype.bigger = function () {
+  let array = this;
+  let biggerNumber = 0;
+  let isNumber = array.some((e) => {
+    return typeof e === "number"
+  });
+
+  if (isNumber) {
+    for (let index = 0; index < array.length; index++) {
+      if (index === 0) {
+        biggerNumber = array[index];
+      }else {
+        if (array[index] > biggerNumber) {
+          biggerNumber = array[index]
+        }
+      }
+      
+    }
+  }
+
+  console.log(biggerNumber);
+  return biggerNumber;
+
+}
+
+Array.prototype.smaller = function() {
+  let array = this;
+  let smallerNumber = 0;
+  let isNumber = array.some((e) => {
+    return typeof e === "number"
+  });
+
+  if (isNumber) {
+    for (let index = 0; index < array.length; index++) {
+      if (index === 0) {
+        smallerNumber = array[index];
+      }else {
+        if (array[index] < smallerNumber) {
+          smallerNumber = array[index]
+        }
+      }
+      
+    }
+  }
+
+  console.log(smallerNumber);
+  return smallerNumber;
+}
+
